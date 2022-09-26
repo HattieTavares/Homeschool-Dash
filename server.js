3cad80b8c79841e285e5fa1ab1439b6ca2df234a
Hattie <82660400+HattieTavares@users.noreply.github.com>
2022-09-26 14:47:13 -0400

    trying to fix image

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ connectDB()
 // Set Middleware
 app.set("view engine", "ejs")
 app.use(express.static("public"))
+app.use('/images', express.static('images'));
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(logger("dev"))
