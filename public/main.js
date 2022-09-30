095f776ca456de45219cefd5d9cea72099a4dbb0
Hattie <82660400+HattieTavares@users.noreply.github.com>
2022-09-29 21:25:51 -0400

    more mobile styling

--- public/main.js
+++ public/main.js
@@ -8,3 +8,13 @@ burger.addEventListener('click', (e) => {
     menu.classList.add('hidden');
   }
 });
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
